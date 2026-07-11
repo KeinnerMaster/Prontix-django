32e713a61972f7ceb8f7e8334f8e77ad60be0656
KeinnerMaster <152994889+KeinnerMaster@users.noreply.github.com>
2026-07-11 18:52:25 -0300

    6:52

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,19 +1,10 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for greenery_project project.
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    # Páginas principales
-    path('', views.index, name='index'),                    # http://127.0.0.1:8000/
-    path('about/', views.about, name='about'),              # http://127.0.0.1:8000/about/
-    path('contact/', views.contact, name='contact'),        # http://127.0.0.1:8000/contact/
-    path('faq/', views.faq, name='faq'),                    # http://127.0.0.1:8000/faq/
-    path('privacy/', views.privacy, name='privacy'),        # http://127.0.0.1:8000/privacy/
-    
-    # Productos
-    path('producto/<slug:slug>/', views.producto_detalle, name='producto_detalle'),  # URLs dinámicas
-    
-    # Carrito y checkout
-    path('carrito/', views.cart, name='cart'),              # http://127.0.0.1:8000/carrito/
-    path('checkout/', views.checkout, name='checkout'),     # http://127.0.0.1:8000/checkout/
-    path('confirmado/', views.order_confirmed, name='order_confirmed'),  # http://127.0.0.1:8000/confirmado/
+    path('admin/', admin.site.urls),
+    path('', include('tienda.urls')),
 ]
