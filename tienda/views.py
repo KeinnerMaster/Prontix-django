0d8d2079ca99a350df69e5bc20a79ac971541d83
KeinnerMaster <152994889+KeinnerMaster@users.noreply.github.com>
2026-07-17 20:42:45 -0300

    Replace send_mail import with resend

--- tienda/views.py
+++ tienda/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from .models import Producto, Categoria, Pedido, ItemPedido
 from .cart import Cart
-from django.core.mail import send_mail
+import resend
 from django.conf import settings
 
 def index(request):
