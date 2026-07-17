93fe58c2d3f31019548aad78e87612596af1b89b
KeinnerMaster <152994889+KeinnerMaster@users.noreply.github.com>
2026-07-17 20:18:25 -0300

    Add email backend configuration to settings.py
    Configure email settings for sending emails via Gmail SMTP.

--- greenery_project/settings.py
+++ greenery_project/settings.py
@@ -115,6 +115,14 @@ if 'RAILWAY_ENVIRONMENT' in os.environ:
             default=os.environ.get('DATABASE_URL'),
             conn_max_age=600
         )
+        EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+    CONTACT_EMAIL_DESTINO = 'imkeinner@gmail.com'
     }
 
     # Archivos estáticos comprimidos con WhiteNoise (sin manifest de hashes,
