81c35d2cb726b8554b16bcb6fc5dfea5ec39a376
KeinnerMaster <152994889+KeinnerMaster@users.noreply.github.com>
2026-07-19 20:18:40 -0300

    Add inline variant management to product admin

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from .models import Categoria, Producto, Variante, Pedido, ItemPedido
 
+
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ['nombre', 'slug']
     prepopulated_fields = {'slug': ('nombre',)}
+
+
+class VarianteInline(admin.TabularInline):
+    """Permite agregar talla/color directamente dentro de la pantalla
+    de edicion del producto, sin entrar a otra seccion."""
+    model = Variante
+    extra = 1
+
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
@@ -13,6 +22,8 @@ class ProductoAdmin(admin.ModelAdmin):
     search_fields = ['nombre', 'descripcion']
     prepopulated_fields = {'slug': ('nombre',)}
     list_editable = ['precio', 'stock', 'destacado', 'activo']
+    inlines = [VarianteInline]
+
 
 class ItemPedidoInline(admin.TabularInline):
     model = ItemPedido
@@ -25,6 +36,7 @@ class ItemPedidoInline(admin.TabularInline):
 class PedidoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre_cliente', 'telefono_cliente', 'total', 'estado', 'creado_en')
     list_filter = ('estado', 'creado_en')
+    list_editable = ('estado',)
     search_fields = ('nombre_cliente', 'email_cliente', 'telefono_cliente')
     readonly_fields = ('nombre_cliente', 'email_cliente', 'telefono_cliente', 'direccion', 'ciudad', 'notas', 'total', 'creado_en')
     inlines = [ItemPedidoInline]
