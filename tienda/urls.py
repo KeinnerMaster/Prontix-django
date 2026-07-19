8a90d1a30385c3da3109f81af1929f6544244a70
KeinnerMaster <152994889+KeinnerMaster@users.noreply.github.com>
2026-07-19 12:12:48 -0300

    Add shipping policy route to URLs

--- tienda/urls.py
+++ tienda/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('faq/', views.faq, name='faq'),
     path('privacy/', views.privacy, name='privacy'),
+    path('politica-de-envio/', views.shipping_policy, name='shipping_policy'),
     path('producto/<slug:slug>/', views.producto_detalle, name='producto_detalle'),
     path('carrito/', views.cart, name='cart'),
     path('carrito/agregar/<int:producto_id>/', views.add_to_cart, name='add_to_cart'),
