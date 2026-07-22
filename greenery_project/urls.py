4e21eda6281fb508c38a54a1cbe334273ddf7717
KeinnerMaster <152994889+KeinnerMaster@users.noreply.github.com>
2026-07-22 17:49:55 -0300

    Add sitemap configuration to urls.py

--- greenery_project/urls.py
+++ greenery_project/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from tienda.sitemaps import ProductoSitemap, PaginasEstaticasSitemap
+
+sitemaps = {
+    'productos': ProductoSitemap,
+    'paginas': PaginasEstaticasSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('', include('tienda.urls')),
 ]
